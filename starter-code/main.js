ef4aa527bf7770b56904d229bc17947f9022cf80
Joshua Whitney <josh.whitney18@gmail.com>
2016-07-31 19:04:05 -0400

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,64 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+/*if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else if (cardThree === cardFour) {
+	alert("You found a match!");
+} else if (cardOne && cardThree); {
+	alert("Sorry, not a match!");
+}
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i = 1; i <= 4; i++) {
+	var card = document.createElement('div');
+	card.className = 'card';
+	gameBoard.appendChild(card);
+	}
+}
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var gameBoard = document.createElement('div');
+var card = document.createElement('div');
+
+f
+}
+
+function createBoard() {
+	for (i = 0; i <=4; i++ ) {
+		document.createElement('div');
+		
+	}
+	
+}
+*/
