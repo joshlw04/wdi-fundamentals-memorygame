f8a23ac69e5273cfb7e2c59d2080c62008d0a8ec
Joshua Whitney <josh.whitney18@gmail.com>
2016-08-08 20:21:01 -0400

    almost doneeeeee
    added images to cards, got them to flip

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,67 +1,50 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
 
-/*if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else if (cardThree === cardFour) {
-	alert("You found a match!");
-} else if (cardOne && cardThree); {
-	alert("Sorry, not a match!");
-}
-*/
-
+//creates game board
 var gameBoard = document.getElementById('game-board');
 
 function createBoard() {
-	for (var i = 1; i <= 4; i++) {
-	var card = document.createElement('div');
-	card.className = 'card';
-	gameBoard.appendChild(card);
+	for (var i = 0; i <= 3; i++) {
+		//this creates the div
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		cardElement.innerHTML = '<img src="images/back.jpg">'
+		gameBoard.appendChild(cardElement);
 	}
 }
 
 createBoard();
 
+//function to see if two cards are in play
+function isTwoCards() {
+	//'this' is grabbing the 'click' from whatever called this function
+	this.innerHTML = '<img src="images/my_' + this.getAttribute('data-card') + '.jpg">'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-var gameBoard = document.createElement('div');
-var card = document.createElement('div');
-
-f
+	//'this' is the same as the line above
+	cardsInPlay.push(this.getAttribute('data-card'));
+	
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
 }
 
-function createBoard() {
-	for (i = 0; i <=4; i++ ) {
-		document.createElement('div');
-		
+//function to alert if player found a match
+function isMatch() {
+	
+	
+	if (cardsInPlay[0] == cardsInPlay[1]) {
+		alert("You found a match!");
+ 		cardsInPlay = [];
+	} else {
+		alert("Sorry, not a match!");
+		cardsInPlay = [];
 	}
 	
+		
 }
-*/
+
